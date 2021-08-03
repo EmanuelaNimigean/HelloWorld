436b9ba97cf923f37011e4ba5de7ec8e90a44ce9
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-03 11:44:25 +0300

    Add team members from backend.

---
+++ HelloWorldWebMVC/Models/TeamInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HelloWorldWebMVC.Models
+{
+    public class TeamInfo
+    {
+        public string Name { get; set; }
+        public List<string> TeamMembers { get; set; }
+    }
+}
