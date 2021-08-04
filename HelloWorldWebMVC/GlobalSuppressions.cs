e7bae5150fbd8720fd59b62b0178079dfc347ded
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-04 10:40:04 +0300

    stylecop applied to both projects

--- HelloWorldWebMVC/GlobalSuppressions.cs
+++ HelloWorldWebMVC/GlobalSuppressions.cs
@@ -4,17 +4,3 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Privacy~Microsoft.AspNetCore.Mvc.IActionResult")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Index~Microsoft.AspNetCore.Mvc.IActionResult")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Error~Microsoft.AspNetCore.Mvc.IActionResult")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.AddTeamMember(System.String)")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.GetCount~System.Int32")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.#ctor(Microsoft.Extensions.Logging.ILogger{HelloWorldWebMVC.Controllers.HomeController})")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:HelloWorldWebMVC.Controllers.HomeController")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:HelloWorldWebMVC.Program")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~P:HelloWorldWebMVC.Models.TeamInfo.TeamMembers")]
-[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1516:Elements should be separated by blank line", Justification = "<Pending>", Scope = "member", Target = "~P:HelloWorldWebMVC.Models.TeamInfo.TeamMembers")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~P:HelloWorldWebMVC.Models.TeamInfo.Name")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:HelloWorldWebMVC.Models.TeamInfo")]
--- HelloWorldWebMVC/Models/TeamInfo.cs
+++ HelloWorldWebMVC/Models/TeamInfo.cs
@@ -5,6 +5,7 @@ namespace HelloWorldWebMVC.Models
     public class TeamInfo
     {
         public string Name { get; set; }
+
         public List<string> TeamMembers { get; set; }
     }
 }
