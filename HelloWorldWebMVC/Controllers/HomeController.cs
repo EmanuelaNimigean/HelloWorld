0ab3aa84a17395e6d79627cc95bd0819981c4420
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-04 10:00:17 +0300

    get rid of warnings

--- HelloWorldWebMVC/Controllers/HomeController.cs
+++ HelloWorldWebMVC/Controllers/HomeController.cs
@@ -1,56 +1,59 @@
-﻿using HelloWorldWebMVC.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿// <copyright file="HomeController.cs" company="Principal33 Solutions">
+// Copyright (c) Principal33 Solutions. All rights reserved.
+// </copyright>
+
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using HelloWorldWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
+// using System.Linq;
+// using System.Threading.Tasks;
 namespace HelloWorldWebMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
         private readonly TeamInfo teamInfo;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
-            teamInfo = new TeamInfo
+            this.logger = logger;
+            this.teamInfo = new TeamInfo
             {
                 Name = "Team1",
-                TeamMembers = new List<string>(new string[] { "Fineas", "Patrick", "Radu", "Tudor", "Ema" })
+                TeamMembers = new List<string>(new string[] { "Fineas", "Patrick", "Radu", "Tudor", "Ema" }),
             };
-
         }
 
         [HttpGet]
         public int GetCount()
         {
-            return teamInfo.TeamMembers.Count;
+            return this.teamInfo.TeamMembers.Count;
         }
 
         [HttpPost]
         public void AddTeamMember(string name)
         {
-            teamInfo.TeamMembers.Add(name);
+            this.teamInfo.TeamMembers.Add(name);
         }
 
         public IActionResult Index()
         {
-            return View(teamInfo);
+            return this.View(this.teamInfo);
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ HelloWorldWebMVC/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Privacy~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Index~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.Error~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.AddTeamMember(System.String)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.GetCount~System.Int32")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:HelloWorldWebMVC.Controllers.HomeController.#ctor(Microsoft.Extensions.Logging.ILogger{HelloWorldWebMVC.Controllers.HomeController})")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:HelloWorldWebMVC.Controllers.HomeController")]
