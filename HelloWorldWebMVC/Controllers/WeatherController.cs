9b71fc55ab297038a0b49036ee5efdd9789458cb
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-12 11:25:15 +0300

    check weather from Cluj

--- HelloWorldWebMVC/Controllers/WeatherController.cs
+++ HelloWorldWebMVC/Controllers/WeatherController.cs
@@ -1,9 +1,8 @@
 ﻿using HelloWorldWebMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,12 +12,25 @@ namespace HelloWorldWebMVC.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        private readonly string latitude= "46.7700";
+        private readonly string longitude= "23.5800";
+        private readonly string apiKey= "cc9172eeab7743b7d5cbc7d65ac03925";
+
         // GET: api/<WeatherController>
         [HttpGet]
         public IEnumerable<DailyWeatherRecord> Get()
         {
+            var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return ConvertResponseToWeatherForecastList(response.Content);
+        }
+
+        private IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherForecastList(string content)
+        {
             return new DailyWeatherRecord[] {
-                new DailyWeatherRecord(new DateTime(2021,8,12),22,WeatherType.Mild)
+                new DailyWeatherRecord(new DateTime(2021,8,12),22,WeatherType.Mild),
             };
         }
 
@@ -29,22 +41,5 @@ namespace HelloWorldWebMVC.Controllers
             return "value";
         }
 
-        // POST api/<WeatherController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<WeatherController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<WeatherController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
