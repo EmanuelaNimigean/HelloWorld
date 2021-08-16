72fb20761d9667dd29da25822f0a3c336bc04c47
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-16 12:21:26 +0300

    Final changes to complete deploy on Heroku

--- HelloWorldWebMVC/Program.cs
+++ HelloWorldWebMVC/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 // using System;
 // using System.Collections.Generic;
@@ -24,7 +25,9 @@ namespace HelloWorldWebMVC
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:" + port);
                 });
     }
 }
