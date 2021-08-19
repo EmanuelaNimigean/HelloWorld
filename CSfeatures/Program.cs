edc0cb816bae67b140633ecd2f889e0685060c71
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-08-19 10:57:12 +0300

    Coffee shop

--- CSfeatures/Program.cs
+++ CSfeatures/Program.cs
@@ -8,7 +8,7 @@ namespace CSfeatures
     {
         static void Main(string[] args)
         {
-            TeamMember teamMember = new TeamMember() { Name="Member1"};
+            TeamMember teamMember = new TeamMember() { Name = "Member1" };
             string jsonString = JsonSerializer.Serialize(teamMember);
             Console.WriteLine(jsonString);
             File.WriteAllText("OutputFile.txt", jsonString);
@@ -17,6 +17,40 @@ namespace CSfeatures
             var awaitOutput = readFromFile.Result;
             var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(awaitOutput);
             Console.WriteLine(teamMemberDeserialized);
+
+            Console.Write("Choose coffee type:");
+            var customerInput = Console.ReadLine();
+            Func<string, string, string, string, Coffee> recipe = (customerInput=="FlatWhite")?FlatWhite:Espresso;
+            Coffee coffee = MakeCoffee("grain", "sugar", "water", "milk", recipe);
+            Console.WriteLine($"Here is your coffee:{coffee}.");
+        }
+
+        static Coffee MakeCoffee(string coffeeGrains,string milk,string water,string sugar,Func<string,string,string,string,Coffee> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffee.");
+                var coffee = recipe(coffeeGrains, milk, water, sugar);
+                return coffee;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+        }
+
+        static Coffee Espresso(string coffeeGrains, string milk, string water, string sugar)
+        {
+            return new Coffee("Espresso");
+        }
+
+        static Coffee FlatWhite(string coffeeGrains, string milk, string water, string sugar)
+        {
+            return new Coffee("FlatWhite");
         }
     }
 }
